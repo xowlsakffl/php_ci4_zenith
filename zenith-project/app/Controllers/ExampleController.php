20536384d7a5e776aed88e7fdb9313bf2370bbf0
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-03-02 16:40:16 +0900

    example 구조 생성

--- zenith-project/app/Controllers/ExampleController.php
+++ zenith-project/app/Controllers/ExampleController.php
@@ -6,8 +6,12 @@ use App\Controllers\BaseController;
 
 class ExampleController extends BaseController
 {
-    public function index()
+    public function view($page = 'test')
     {
-        //
+        if(!is_file(APPPATH . 'Views/example/' . $page . '.php')) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        return view('example/'.$page);
     }
 }
