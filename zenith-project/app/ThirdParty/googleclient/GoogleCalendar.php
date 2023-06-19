5c9dd9926c6931baf5f2c68e097c3d1941735b96
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-06-19 12:00:18 +0900

    Create GoogleCalendar.php

---
+++ zenith-project/app/ThirdParty/googleclient/GoogleCalendar.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\ThirdParty\googleclient;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Google\Client;
+use Google\Service\Drive;
+
+class GoogleCalendar
+{
+    protected $client, $redirect_uri, $accessToken;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->client->setApplicationName("Zenith_Client_Library");
+        $this->client->setAuthConfig(__DIR__.'/credentials.json');
+        $this->client->addScope(Drive::DRIVE);
+        $this->redirect_uri = 'https://carezenith.co.kr/calendar/oauth';
+        $this->client->setRedirectUri($this->redirect_uri);
+
+        if (!isset($_GET['code'])) {
+            $authUrl = $this->client->createAuthUrl();
+            header('Location: ' . $authUrl);
+            exit;
+        } else {
+            $this->client->fetchAccessTokenWithAuthCode($_GET['code']);
+            $this->accessToken = $this->client->getAccessToken();
+        }
+    }
+}
