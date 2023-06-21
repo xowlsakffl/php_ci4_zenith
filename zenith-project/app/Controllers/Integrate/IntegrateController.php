83354f1ac66201d0fc6cd5a9ec27d3501e6ce018
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-06-21 11:37:48 +0900

    통합 DB 관리 메소드 수정

--- zenith-project/app/Controllers/Integrate/IntegrateController.php
+++ zenith-project/app/Controllers/Integrate/IntegrateController.php
@@ -24,7 +24,7 @@ class IntegrateController extends BaseController
 
     public function getList()
     {
-        if($this->request->isAJAX() && strtolower($this->request->getMethod()) === 'get'){
+        if(/* $this->request->isAJAX() &&  */strtolower($this->request->getMethod()) === 'get'){
             $arg = $this->request->getGet();
             if(!isset($arg['searchData'])) {
                 $arg['searchData'] = [
@@ -72,41 +72,39 @@ class IntegrateController extends BaseController
                 $row['add'][] = $add;
             }
 
-            //buttons
             $data = $this->integrate->getEventLeadCount($arg);
+            $status = $this->integrate->getStatusCount($arg);
 
             $adv_counts = array();
             $med_counts = array();
             $event_counts = array();
-            foreach ($data as $row) {
-                if (!array_key_exists($row['adv_name'], $adv_counts)) {
-                    $adv_counts[$row['adv_name']] = array(
+            foreach ($data as $d) {
+                if (!array_key_exists($d['adv_name'], $adv_counts)) {
+                    $adv_counts[$d['adv_name']] = array(
                         'countAll' => 0
                     );
                 }
 
-                if (!array_key_exists($row['med_name'], $med_counts)) {
-                    $med_counts[$row['med_name']] = array(
+                if (!array_key_exists($d['med_name'], $med_counts)) {
+                    $med_counts[$d['med_name']] = array(
                         'countAll' => 0
                     );
                 }
 
-                $event_counts[$row['event']] = array(
-                    'countAll' => $row['countAll'],
+                $event_counts[$d['event']] = array(
+                    'countAll' => $d['countAll'],
                 );
 
-                $adv_counts[$row['adv_name']]['countAll'] += $row['countAll'];
-                $med_counts[$row['med_name']]['countAll'] += $row['countAll'];
+                $adv_counts[$d['adv_name']]['countAll'] += $d['countAll'];
+                $med_counts[$d['med_name']]['countAll'] += $d['countAll'];
             }
 
             $buttons = [
                 'advertiser' => $adv_counts,
                 'media' => $med_counts,
                 'event' => $event_counts,
+                'status' => $status,
             ];
-
-            //status
-            $status = $this->integrate->getStatusCount($arg);
 
             $result = [
                 'data' => $result['data'],
@@ -114,7 +112,6 @@ class IntegrateController extends BaseController
                 'recordsFiltered' => $result['allCount'],
                 'draw' => intval($arg['draw']),
                 'buttons' => $buttons,
-                'status' => $status,
             ];
 
             return $this->respond($result);
