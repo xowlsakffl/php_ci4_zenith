75b149f84b2a6134d7f266a8d0dc752fff09a9bf
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-08-23 18:22:20 +0900

    Update magic_link_email.php

--- zenith-project/app/Views/auth/email/magic_link_email.php
+++ zenith-project/app/Views/auth/email/magic_link_email.php
@@ -15,29 +15,29 @@
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="background-color: #f6f6f6; width: 100%;">
       <tr>
         <td>&nbsp;</td>
-        <td class="container" style="display: block; margin: 0 auto !important; max-width: 580px; padding: 10px; width: 580px;text-align:center">
+        <td class="container" style="display: block; margin: 0 auto !important; max-width: 580px; padding: 10px; width: 580px;">
 
-          <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px;">
+          <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px;background:#ffffff">
 
             <!-- START CENTERED WHITE CONTAINER -->
-            <table role="presentation" class="main" style="background: #ffffff; border-radius: 3px; width: 100%;margin-bottom:20px">
+            <table role="presentation" class="main" style="border-radius: 3px; width: 100%;margin-bottom:20px">
 
               <!-- START MAIN CONTENT AREA -->
               <tr>
                 <td class="wrapper">
-                  <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;">
                     <tr>
                       <td>
-                        <h2 style="color: #000000; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0; margin-bottom: 30px; font-size: 35px; text-align: center; text-transform: capitalize;">비밀번호 재설정 링크</h2>
+                        <h3 style="color: #000000; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0; margin-bottom: 20px; font-size: 35px; text-align: center; text-transform: capitalize;">비밀번호 재설정 링크</h3>
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">아래 버튼을 클릭하여 접속 후 비밀번호를 변경해주시기 바랍니다.</p>
                         <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="box-sizing: border-box; width: 100%;">
                           <tbody>
                             <tr>
                               <td align="left">
-                                <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="margin: 0 auto;">
                                   <tbody>
                                     <tr>
-                                      <td> <a href="<?= url_to('verify-magic-link') ?>?token=<?= $token ?>" target="_blank" style="background-color: #ce1922; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; color: #ffffff; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-decoration: none; text-transform: capitalize;">비밀번호 재설정</a></td>
+                                      <td> <a href="<?= url_to('verify-magic-link') ?>?token=<?= $token ?>" target="_blank" style="background-color: #ce1922; border-radius: 5px; box-sizing: border-box; color: #ffffff; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-decoration: none; text-transform: capitalize;">비밀번호 재설정</a></td>
                                     </tr>
                                   </tbody>
                                 </table>
