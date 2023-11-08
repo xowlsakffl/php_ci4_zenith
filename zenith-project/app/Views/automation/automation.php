7dd2ca115af94bc7d1e6c71ef2c33cdab05aae74
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-11-08 17:21:14 +0900

    Update automation.php

--- zenith-project/app/Views/automation/automation.php
+++ zenith-project/app/Views/automation/automation.php
@@ -496,7 +496,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <table class="table tbl-dark" id="logTable">
+                <table class="table tbl-dark" id="logTable" style="width: 100%;">
                     <colgroup>
                         <col>
                         <col style="width:22%;">
