18492ef9e063333b124024525ae7af16117d2243
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-11-15 10:55:32 +0900

    Update JiraController.php

--- zenith-project/app/Controllers/Api/JiraController.php
+++ zenith-project/app/Controllers/Api/JiraController.php
@@ -36,9 +36,9 @@ class JiraController extends BaseController
     {  
         $logText = '';
         $logText .= '요청 시간: '.date('Y-m-d H:i:s')."\n";
-        $logText .= '요청 헤더: '.json_encode($this->request->headers())."\n";
-        $logText .= '요청 메소드: '.json_encode($this->request->getMethod())."\n";
-        $logText .= '요청 바디: '.json_encode($this->request->getBody());
+        $logText .= '요청 헤더: '.$this->request->headers()."\n";
+        $logText .= '요청 메소드: '.$this->request->getMethod()."\n";
+        $logText .= '요청 바디: '.$this->request->getBody();
         $this->writeLog($logText);
         if (strtolower($this->request->getMethod()) === 'post') {
             $param = $this->request->getVar();
