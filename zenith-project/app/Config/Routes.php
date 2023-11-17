b4fd46f54ae476d5dec00e4621ee16ebdaee2069
msan <50791439+xowlsakffl@users.noreply.github.com>
2023-11-17 11:00:09 +0900

    Update Routes.php

--- zenith-project/app/Config/Routes.php
+++ zenith-project/app/Config/Routes.php
@@ -54,7 +54,7 @@ $routes->group('', ['filter' => 'group:admin,superadmin,developer,user,agency,ad
     });
 
     //광고주, 광고대행사 관리
-    $routes->group('', ['filter' => 'group:group:superadmin,admin,developer,user', 'permission:admin.access,admin.settings,agency.access'], static function($routes){
+    $routes->group('', ['filter' => 'group:superadmin,admin,developer,user', 'permission:admin.access,admin.settings,agency.access'], static function($routes){
         $routes->get('company', 'Company\CompanyController::index');
         $routes->get('company/get-companies', 'Company\CompanyController::getCompanies');
         $routes->get('company/get-company', 'Company\CompanyController::getCompany');
