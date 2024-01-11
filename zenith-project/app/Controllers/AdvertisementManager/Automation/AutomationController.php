7675c0510f5ac8d0c34381f9afb90790684c00b0
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-01-11 14:02:49 +0900

    Update AutomationController.php

--- zenith-project/app/Controllers/AdvertisementManager/Automation/AutomationController.php
+++ zenith-project/app/Controllers/AdvertisementManager/Automation/AutomationController.php
@@ -550,9 +550,6 @@ class AutomationController extends BaseController
         $total = count($automations);
         foreach ($automations as $automation) {
             $result = [];
-            if($automation['aa_seq'] != '95'){
-                continue;
-            }
             if(!empty($automation)){
                 $schedulePassData = $this->checkAutomationSchedule($automation);
                 $result['schedule'] = $schedulePassData;
